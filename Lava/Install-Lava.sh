d42f7c992bf9252fe65e7297fe7424803b573fa6
Asapov <137000528+asapov01@users.noreply.github.com>
2024-03-30 20:47:27 +0200

    Update Install-Lava.sh

--- Lava/Install-Lava.sh
+++ Lava/Install-Lava.sh
@@ -12,7 +12,7 @@ function install() {
   clear
   source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 
-  printGreen "Введіть ім'я для вашої ноди(Наприклад:Oliver111111):"
+  printGreen "Введіть ім'я для вашої ноди(Наприклад:2222):"
   read -r NODE_MONIKER
 
   CHAIN_ID="lava-testnet-2"
