4a34365075bd5679eb6d9b77e0fc90208d12b9ec
Asapov <137000528+asapov01@users.noreply.github.com>
2024-07-20 01:36:59 +0300

    Create Delete-0GStorage.sh

---
+++ 0G-Storage/Delete-0GStorage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+function logo() {
+  bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+}
+
+function delete() {
+  sudo systemctl stop zgs.service
+  sudo systemctl disable zgs.service
+  sudo rm -rf /etc/systemd/system/zgs.service
+  sudo rm -rf $HOME/0g-storage-node
+  sudo rm -rf /usr/local/bin/zgs_node
+}
+
+logo
+delete
+
+printGreen "0G node видалено"
