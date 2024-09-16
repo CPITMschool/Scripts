88ea9cdedb92e719df9f85f7abdb6a76a975a238
Fearless Oleksii <143862878+0xFearless-1@users.noreply.github.com>
2024-09-16 09:33:19 +0300

    Update Install-0G.sh

--- 0G/Install-0G.sh
+++ 0G/Install-0G.sh
@@ -59,8 +59,8 @@ sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.0gchain/config/config.tom
 
 ### Downoload snapshot
 0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain
-if curl -s --head curl https://server-5.itrocket.net/testnet/og/og_2024-09-15_1071573_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
-  curl https://server-5.itrocket.net/testnet/og/og_2024-09-15_1071573_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
+if curl -s --head curl https://server-5.itrocket.net/testnet/og/og_2024-09-16_1083945_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://server-5.itrocket.net/testnet/og/og_2024-09-16_1083945_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
     else
   echo "no snapshot founded"
 fi
