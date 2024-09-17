21d5c3c054fc94dd9c1051466e588178f5eec37a
Fearless Oleksii <143862878+0xFearless-1@users.noreply.github.com>
2024-09-17 16:15:03 +0300

    Update Install-Storage.sh

--- Storage/Install-Storage.sh
+++ Storage/Install-Storage.sh
@@ -60,7 +60,7 @@ printColor blue "Download Snapshots"
 cd $HOME
 wget --show-progress https://snapshots-testnet.unitynodes.com/0gchain-testnet/storage_0gchain_snapshot.lz4
 rm -rf $HOME/0g-storage-node/run/{db,log,network}
-lz4 -c -d 1609_storage_0gchain_snapshot.lz4 | pv | tar -x -C $HOME/0g-storage-node/run
+lz4 -c -d storage_0gchain_snapshot.lz4 | pv | tar -x -C $HOME/0g-storage-node/run
 
 
 printColor blue "Node Configuration"
