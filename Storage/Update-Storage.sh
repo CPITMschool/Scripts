87e09c7ec368c95fa0f0dafeb0a73813e39b4c9a
Fearless Oleksii <143862878+0xFearless-1@users.noreply.github.com>
2024-10-14 13:46:38 +0300

    Update Update-Storage.sh

--- Storage/Update-Storage.sh
+++ Storage/Update-Storage.sh
@@ -10,34 +10,28 @@ logo
 printColor blue "Stop node zgs" && sleep 1
 sudo systemctl stop zgs
 
+printColor blue "Backup config.toml" && sleep 1
+cp $HOME/0g-storage-node/run/config-testnet-turbo.toml $HOME/0g-storage-node/run/config-testnet-turbo.toml.backup
+
 printColor blue "Setup update" && sleep 1
-sudo apt-get update
-sudo apt-get openssl libssl-dev pkg-config
-sudo apt-get install wget lz4 aria2 pv -y
-
-printColor blue "Backup config file" && sleep 1
-mv $HOME/0g-storage-node/run/config-testnet-turbo.toml $HOME/config-testnet-turbo_backup.toml
-
-printColor blue "Update 0G Storage"
 cd $HOME/0g-storage-node
+git stash
 git fetch --all --tags
-git checkout v0.5.1
+git checkout 3fc1543
 git submodule update --init
 cargo build --release
+cp $HOME/0g-storage-node/run/config.toml.backup $HOME/0g-storage-node/run/config.toml
+sudo systemctl daemon-reload
+sudo systemctl enable zgs
+sudo systemctl restart zgs
 
-printColor blue "Download Snapshots"
-cd $HOME
-wget --show-progress https://snapshots-testnet.unitynodes.com/0gchain-testnet/storage_0gchain_snapshot.lz4
-rm -rf $HOME/0g-storage-node/run/{db,log,network}
-lz4 -c -d storage_0gchain_snapshot.lz4 | pv | tar -x -C $HOME/0g-storage-node/run
-
-printColor blue "Restore backup"
-mv $HOME/config-testnet-turbo_backup.toml $HOME/0g-storage-node/run/config-testnet-turbo.toml
-BLOCKCHAIN_RPC_ENDPOINT="https://evm-rpc.0gchain-testnet.unitynodes.com"
-
-sed -i "s|^blockchain_rpc_endpoint = \".*\"|blockchain_rpc_endpoint = \"$BLOCKCHAIN_RPC_ENDPOINT\"|" $HOME/0g-storage-node/run/config-testnet-turbo.toml
-
-sudo systemctl restart zgs && sudo systemctl status zgs
+while true; do
+    response=$(curl -s -X POST http://localhost:5678 -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"zgs_getStatus","params":[],"id":1}')
+    logSyncHeight=$(echo $response | jq '.result.logSyncHeight')
+    connectedPeers=$(echo $response | jq '.result.connectedPeers')
+    echo -e "logSyncHeight: \033[32m$logSyncHeight\033[0m, connectedPeers: \033[34m$connectedPeers\033[0m"
+    sleep 5
+done
 
 echo ""
 printLine
