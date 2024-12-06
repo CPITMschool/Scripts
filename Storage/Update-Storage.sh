83ea4fe6ebefa7002c4a5bdeb8eb3ed9d46281a2
Fearless Oleksii <143862878+0xFearless-1@users.noreply.github.com>
2024-12-06 09:43:17 +0200

    Update Update-Storage.sh

--- Storage/Update-Storage.sh
+++ Storage/Update-Storage.sh
@@ -9,52 +9,65 @@ logo
 
 #!/bin/bash
 
-set -e
-
-# Зупиняємо ноду та оновлюємо залежності
-echo "Зупиняємо ноду та оновлюємо залежності..."
+# Зупинка сервісу
+echo "Зупинка сервісу zgs..."
 sudo systemctl stop zgs
+
+# Оновлення системних пакетів
+echo "Оновлення системних пакетів..."
 sudo apt-get update
 sudo apt-get install -y openssl libssl-dev pkg-config
 
-# Видаляємо старі дані і робимо бекап конфіг файлу
-echo "Видаляємо старі дані і робимо бекап конфіг файлу..."
-cd $HOME
+# Видалення старої бази даних
+echo "Видалення старої бази даних..."
 rm -rf $HOME/0g-storage-node/run/db
+
+# Резервне копіювання файлу конфігурації
+echo "Створення резервної копії файлу конфігурації..."
 mv $HOME/0g-storage-node/run/config-testnet-turbo.toml $HOME/config-testnet-turbo_backup.toml
 
-# Клонуємо і створюємо новий бінарний файл
-echo "Клонуємо і створюємо новий бінарний файл..."
+# Оновлення репозиторію
+echo "Оновлення репозиторію 0g-storage-node..."
 cd $HOME/0g-storage-node
 git fetch --all --tags
 git checkout v0.8.0
 git submodule update --init
+
+# Компіляція проекту
+echo "Компіляція проекту..."
 cargo build --release
 
-# Відновлюємо конфіг файл
-echo "Відновлюємо конфіг файл..."
+# Відновлення файлу конфігурації
+echo "Відновлення файлу конфігурації..."
 mv $HOME/config-testnet-turbo_backup.toml $HOME/0g-storage-node/run/config-testnet-turbo.toml
 
+# Оновлення та встановлення необхідних утиліт
+echo "Встановлення необхідних утиліт..."
+sudo apt-get update
+sudo apt-get install -y wget lz4 aria2 pv
 
-# Перевіряємо версію сторедж ноди
-echo "Перевіряємо версію сторедж ноди..."
-cd $HOME/0g-storage-node
-git log --decorate=short --oneline | grep "tag: v" | head -n 1
-git log -1 --pretty=oneline
-
-# Міняємо RPC
-echo "Міняємо RPC..."
-BLOCKCHAIN_RPC_ENDPOINT="https://16600.rpc.thirdweb.com"
-sed -i "s|^blockchain_rpc_endpoint = \".*\"|blockchain_rpc_endpoint = \"$BLOCKCHAIN_RPC_ENDPOINT\"|" $HOME/0g-storage-node/run/config-testnet-turbo.toml
-
-# Грузимо снепшот
+# Завантаження та розпаковування знімка
+echo "Завантаження та розпакування знімка блокчейну..."
 cd $HOME
-rm storage_0gchain_snapshot.lz4
+rm -f storage_0gchain_snapshot.lz4
 aria2c -x 16 -s 16 -k 1M https://josephtran.co/storage_0gchain_snapshot.lz4
 rm -rf $HOME/0g-storage-node/run/db
 lz4 -c -d storage_0gchain_snapshot.lz4 | pv | tar -x -C $HOME/0g-storage-node/run
 
+# Оновлення файлу конфігурації
+echo "Оновлення файлу конфігурації з новими вузлами та RPC-ендпоінтом..."
+sed -i 's|^network_boot_nodes = .*|network_boot_nodes = ["/ip4/47.251.117.133/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/47.76.61.226/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"]|g' $HOME/0g-storage-node/run/config-testnet-turbo.toml
+
+BLOCKCHAIN_RPC_ENDPOINT="https://16600.rpc.thirdweb.com"
+sed -i "s|^blockchain_rpc_endpoint = \".*\"|blockchain_rpc_endpoint = \"$BLOCKCHAIN_RPC_ENDPOINT\"|" $HOME/0g-storage-node/run/config-testnet-turbo.toml
+
+# Перезапуск сервісу
+echo "Перезапуск сервісу zgs..."
 sudo systemctl restart zgs
-tail -f ~/0g-storage-node/run/log/zgs.log.$(TZ=UTC date +%Y-%m-%d)
+
+# Перегляд логів
+echo "Виведення логів сервісу zgs..."
+tail -f $HOME/0g-storage-node/run/log/zgs.log.$(TZ=UTC date +%Y-%m-%d)
+
 
 update
