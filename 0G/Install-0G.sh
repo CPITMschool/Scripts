667bb39556f47123cff70ba362cc8426c411d810
Fearless Oleksii <andrienkoj7@gmail.com>
2025-04-08 17:59:47 +0300

    update 0G

--- 0G/Install-0G.sh
+++ 0G/Install-0G.sh
@@ -26,7 +26,7 @@ printColor blue "[4/6] Building binaries"
 # Clone project repository
 cd $HOME
 rm -rf 0g-chain
-wget -O 0gchaind https://github.com/0glabs/0g-chain/releases/download/v0.5.3/0gchaind-linux-v0.5.3
+wget -O 0gchaind https://github.com/0glabs/0g-chain/releases/download/v0.5.1/0gchaind-linux-v0.5.1
 chmod +x $HOME/0gchaind
 sudo mkdir -p "$HOME/go/bin" && sudo mv "$HOME/0gchaind" "$HOME/go/bin"
 0gchaind version
@@ -43,9 +43,8 @@ wget -O $HOME/.0gchain/config/genesis.json https://server-5.itrocket.net/testnet
 wget -O $HOME/.0gchain/config/addrbook.json  https://server-5.itrocket.net/testnet/og/addrbook.json
 
 SEEDS="bac83a636b003495b2aa6bb123d1450c2ab1a364@og-testnet-seed.itrocket.net:47656"
-PEERS="80fa309afab4a35323018ac70a40a446d3ae9caf@og-testnet-peer.itrocket.net:11656,0b7f6b17f9c96705ccf12261f822af71deddaf47@75.119.152.114:26656,898a1aeb8b8fc47dddf8624bbc4199b9b9000ae8@62.169.29.104:12656,d0b72e81ad02869c94d71771cae303cbe1d92ba7@158.220.107.134:12656,6c04ced3f2ea5dbe53cd6021772f953f19904099@194.163.135.175:12656,a8eb33fc7928dbc72133b39cbe6be11cd730100f@62.171.128.133:12656,342c70d258f65cfa662a1a8e3215627d7c670053@84.46.248.204:26656,338933cdb9cbbc6f4f6195be722726706b2b7e70@194.163.187.249:47656,8e3f23df98376af21eb20fe30fe342395e1418f1@164.68.121.195:12656,fdd67e39de882dbdd43c3df752a96cff9176b56e@158.220.99.250:12656,571d4e154b4f9775904a9256da9d1e9eb36cd84b@185.209.228.106:12656,fd752d52b9b09f12801512119c8e4cc2b6b63501@38.242.140.38:12656,8c5013fcb40149567d38d4e9484586a85b2c1b49@62.171.153.87:12656,cb73a5dd8c6d93cd3223799fe62c03a444082ed1@185.237.253.52:47656,709311571412185ef643bd2773167a5e58d940f1@5.182.17.178:12656,c3fe952bd71aeaa640c432dea2efbf3226fa3208@45.67.216.248:12656,d9443e48bbe44fa711d04c00c7e109857be33381@217.76.52.206:12656,528fd49ac79811239695eb1c4c9eadc430bd2bf2@65.108.36.150:26656,870c7c5a08bc658b896f605e593dacbb23106efa@86.48.0.88:12656,d965c45e1e2ae18cb4db25dbf3159f3b5911e4f0@207.180.212.116:12656,897d11f1bb31aae0e788620768eec70117b2a026@144.91.106.32:12656"
-sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
-       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.0gchain/config/config.toml
+PEERS="80fa309afab4a35323018ac70a40a446d3ae9caf@og-testnet-peer.itrocket.net:11656,bf924ae011cb3f4f5c0c6a1993703fda2ed90661@194.163.160.15:26656,528fd49ac79811239695eb1c4c9eadc430bd2bf2@65.108.36.150:26656,f40437f2be5141371100cdda486c0723710144cd@88.198.70.23:27856,7cf4c84a6e44e919a74f26c04c3bf231fd2d4d03@162.55.98.31:26656,1a01f35a46c6545bc55a8b38f51aed123659f51a@95.216.98.122:11656,044a26f8b424eadc0509c503d3e902552789fef2@37.27.60.37:26656,021e5ca1ca04631790feda8142b05640916a3c6c@5.9.141.187:26656,8554b5583aa2773dd06138388bdfdc25b6038fc6@162.55.102.52:26656,78083fc2c1f98e849ff2969467b82ca154979cdd@135.181.231.88:26656,3fbd35d9137ce0e97a78f9797a5c76debe3f2384@95.217.205.92:26656,779acfc7fc7d45865c1f2652e8bd329e29f36b9f@135.181.57.240:26656,4c7b139e9ec5671887ba66e0f5ff8c1192cf8f43@65.21.82.218:26656,ff8534292b54e69db55f1486adeca9dde9335137@37.27.118.142:26656,2dcc4da1d33ab1db810eea82f978fa145b432d18@168.119.10.33:26656,0c2d883524434768b5df39c71749651acdd620c8@95.217.140.187:26656,8c8ff8aa063d3d2dbcd6a7bf121270aa5dbc12f8@65.109.49.115:27856"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.0gchain/config/config.toml
 
 # Сonfig pruning
 sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.0gchain/config/app.toml 
@@ -60,8 +59,8 @@ sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.0gchain/config/config.tom
 
 ### Downoload snapshot
 0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain
-if curl -s --head curl https://server-5.itrocket.net/testnet/og/og_2025-02-23_3356409_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
-  curl https://server-5.itrocket.net/testnet/og/og_2025-02-23_3356409_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
+if curl -s --head curl https://server-5.itrocket.net/testnet/og/og_2025-04-08_3908831_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://server-5.itrocket.net/testnet/og/og_2025-04-08_3908831_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
     else
   echo "no snapshot found"
 fi
