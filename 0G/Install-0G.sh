63d6830ae8c14f68b87d87fea3894450183801c3
Fearless Oleksii <andrienkoj7@gmail.com>
2025-05-25 15:43:32 +0300

    change install 0G

--- 0G/Install-0G.sh
+++ 0G/Install-0G.sh
@@ -8,94 +8,154 @@ function printGreen {
   echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
-function install() {
-clear
-source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
-
-echo -e "\e[30;47m Введіть ім'я moniker(Наприклад: Oliver):\e[0m"
-echo -en ">>> "
-read -r NODE_MONIKER
-
-### Install Dependencies
+# Install dependencies
 source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/dependencies.sh)
 
-### Building binaries
-echo ""
-printColor blue "[4/6] Building binaries"
+function install() {
+  clear
+  source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 
-# Clone project repository
-cd $HOME
-rm -rf 0g-chain
-wget -O 0gchaind https://github.com/0glabs/0g-chain/releases/download/v0.5.1/0gchaind-linux-v0.5.1
-chmod +x $HOME/0gchaind
-sudo mkdir -p "$HOME/go/bin" && sudo mv "$HOME/0gchaind" "$HOME/go/bin"
-0gchaind version
+  printGreen "Updating packages and installing dependencies..."
+  sudo apt update && sudo apt upgrade -y
+  sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
 
-0gchaind config chain-id zgtendermint_16600-2
-0gchaind config keyring-backend test
-0gchaind config node tcp://localhost:26657
-source $HOME/.bash_profile
+  printGreen "Installing Go..."
+  cd $HOME && \
+  ver="1.22.0" && \
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+  sudo rm -rf /usr/local/go && \
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+  rm "go$ver.linux-amd64.tar.gz" && \
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
+  source ~/.bash_profile && \
+  go version
 
-0gchaind init "$NODE_MONIKER" --chain-id zgtendermint_16600-2
+  printGreen "Setting environment variables..."
+  echo "export MONIKER=\"YourMoniker\"" >> $HOME/.bash_profile
+  echo "export OG_PORT=\"55\"" >> $HOME/.bash_profile
+  echo 'export PATH=$PATH:$HOME/galileo/bin' >> $HOME/.bash_profile
+  source $HOME/.bash_profile
 
-### Download genesis and addrbook
-wget -O $HOME/.0gchain/config/genesis.json https://server-5.itrocket.net/testnet/og/genesis.json
-wget -O $HOME/.0gchain/config/addrbook.json  https://server-5.itrocket.net/testnet/og/addrbook.json
+  printGreen "Downloading Galileo..."
+  cd $HOME
+  rm -rf galileo
+  wget https://github.com/0glabs/0gchain-NG/releases/download/v1.1.1/galileo-v1.1.1.tar.gz
+  tar -xzvf galileo-v1.1.1.tar.gz -C $HOME
+  rm galileo-v1.1.1.tar.gz
+  chmod +x $HOME/galileo/bin/geth
+  chmod +x $HOME/galileo/bin/0gchaind
 
-SEEDS="bac83a636b003495b2aa6bb123d1450c2ab1a364@og-testnet-seed.itrocket.net:47656"
-PEERS="80fa309afab4a35323018ac70a40a446d3ae9caf@og-testnet-peer.itrocket.net:11656,bf924ae011cb3f4f5c0c6a1993703fda2ed90661@194.163.160.15:26656,528fd49ac79811239695eb1c4c9eadc430bd2bf2@65.108.36.150:26656,f40437f2be5141371100cdda486c0723710144cd@88.198.70.23:27856,7cf4c84a6e44e919a74f26c04c3bf231fd2d4d03@162.55.98.31:26656,1a01f35a46c6545bc55a8b38f51aed123659f51a@95.216.98.122:11656,044a26f8b424eadc0509c503d3e902552789fef2@37.27.60.37:26656,021e5ca1ca04631790feda8142b05640916a3c6c@5.9.141.187:26656,8554b5583aa2773dd06138388bdfdc25b6038fc6@162.55.102.52:26656,78083fc2c1f98e849ff2969467b82ca154979cdd@135.181.231.88:26656,3fbd35d9137ce0e97a78f9797a5c76debe3f2384@95.217.205.92:26656,779acfc7fc7d45865c1f2652e8bd329e29f36b9f@135.181.57.240:26656,4c7b139e9ec5671887ba66e0f5ff8c1192cf8f43@65.21.82.218:26656,ff8534292b54e69db55f1486adeca9dde9335137@37.27.118.142:26656,2dcc4da1d33ab1db810eea82f978fa145b432d18@168.119.10.33:26656,0c2d883524434768b5df39c71749651acdd620c8@95.217.140.187:26656,8c8ff8aa063d3d2dbcd6a7bf121270aa5dbc12f8@65.109.49.115:27856"
-sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.0gchain/config/config.toml
+  sudo cp $HOME/galileo/bin/geth /usr/local/bin/geth
+  sudo cp $HOME/galileo/bin/0gchaind /usr/local/bin/0gchaind
 
-# Сonfig pruning
-sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.0gchain/config/app.toml 
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.0gchain/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.0gchain/config/app.toml
+  printGreen "Initializing the chain..."
+  mkdir -p $HOME/.0gchaind
+  cp -r $HOME/galileo $HOME/.0gchaind/
+  geth init --datadir $HOME/.0gchaind/galileo/0g-home/geth-home $HOME/.0gchaind/galileo/genesis.json
+  0gchaind init $MONIKER --home $HOME/.0gchaind/tmp
 
-# Set minimum gas price, enable prometheus and disable indexing
-sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0ua0gi"|g' $HOME/.0gchain/config/app.toml
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.0gchain/config/config.toml
-sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.0gchain/config/config.toml
+  cp $HOME/.0gchaind/tmp/data/priv_validator_state.json $HOME/.0gchaind/galileo/0g-home/0gchaind-home/data/
+  cp $HOME/.0gchaind/tmp/config/node_key.json $HOME/.0gchaind/galileo/0g-home/0gchaind-home/config/
+  cp $HOME/.0gchaind/tmp/config/priv_validator_key.json $HOME/.0gchaind/galileo/0g-home/0gchaind-home/config/
 
+  printGreen "Downloading and extracting snapshot..."
+  mkdir -p ~/snapshot
+  cd ~/snapshot
 
-### Downoload snapshot
-0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain
-if curl -s --head curl https://server-5.itrocket.net/testnet/og/og_2025-04-08_3908831_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
-  curl https://server-5.itrocket.net/testnet/og/og_2025-04-08_3908831_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
-    else
-  echo "no snapshot found"
-fi
+  wget https://vault.astrostake.xyz/0g-labs/validator-snapshot/0gchaind_snapshot_20250520-223437.tar.gz
+  wget https://vault.astrostake.xyz/0g-labs/validator-snapshot/geth_snapshot_20250520-223437.tar.gz
 
+  sudo systemctl stop 0gchaind || true
+  sudo systemctl stop geth || sudo systemctl stop 0ggeth || true
 
-### Create service
-sudo tee /etc/systemd/system/0gchaind.service > /dev/null <<EOF
+  mv ~/.0gchaind/0g-home/0gchaind-home/data ~/.0gchaind/0g-home/0gchaind-home/data.bak.$(date +%s) || true
+  mv ~/.0gchaind/0g-home/geth-home/geth ~/.0gchaind/0g-home/geth-home/geth.bak.$(date +%s) || true
+
+  rm -rf ~/.0gchaind/0g-home/0gchaind-home/data
+  rm -rf ~/.0gchaind/0g-home/geth-home/geth
+
+  tar -xzvf 0gchaind_snapshot_20250520-223437.tar.gz -C ~/.0gchaind/0g-home/
+  tar -xzvf geth_snapshot_20250520-223437.tar.gz -C ~/.0gchaind/0g-home/
+
+  printGreen "Configuring ports and systemd services..."
+
+  CONFIG="$HOME/.0gchaind/galileo/0g-home/0gchaind-home/config"
+
+  sed -i "s/laddr = \"tcp:\/\/0.0.0.0:26656\"/laddr = \"tcp:\/\/0.0.0.0:${OG_PORT}656\"/" $CONFIG/config.toml
+  sed -i "s/laddr = \"tcp:\/\/127.0.0.1:26657\"/laddr = \"tcp:\/\/127.0.0.1:${OG_PORT}657\"/" $CONFIG/config.toml
+  sed -i "s/^proxy_app = .*/proxy_app = \"tcp:\/\/127.0.0.1:${OG_PORT}658\"/" $CONFIG/config.toml
+  sed -i "s/^pprof_laddr = .*/pprof_laddr = \"0.0.0.0:${OG_PORT}060\"/" $CONFIG/config.toml
+  sed -i "s/prometheus_listen_addr = \".*\"/prometheus_listen_addr = \"0.0.0.0:${OG_PORT}660\"/" $CONFIG/config.toml
+
+  sed -i "s/address = \".*:3500\"/address = \"127.0.0.1:${OG_PORT}500\"/" $CONFIG/app.toml
+  sed -i "s/^rpc-dial-url *=.*/rpc-dial-url = \"http:\/\/localhost:${OG_PORT}551\"/" $CONFIG/app.toml
+  sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $CONFIG/config.toml
+
+  sed -i "s/HTTPPort = .*/HTTPPort = ${OG_PORT}545/" $HOME/.0gchaind/galileo/geth-config.toml
+  sed -i "s/WSPort = .*/WSPort = ${OG_PORT}546/" $HOME/.0gchaind/galileo/geth-config.toml
+  sed -i "s/AuthPort = .*/AuthPort = ${OG_PORT}551/" $HOME/.0gchaind/galileo/geth-config.toml
+  sed -i "s/ListenAddr = .*/ListenAddr = \":${OG_PORT}303\"/" $HOME/.0gchaind/galileo/geth-config.toml
+
+  sudo tee /etc/systemd/system/0gchaind.service > /dev/null <<EOF
 [Unit]
-Description=0G node
+Description=0gchaind Node Service
 After=network-online.target
+
 [Service]
 User=$USER
-WorkingDirectory=$HOME/.0gchain
-ExecStart=$(which 0gchaind) start --home $HOME/.0gchain --log_output_console
-Restart=on-failure
-RestartSec=5
+Environment=CHAIN_SPEC=devnet
+WorkingDirectory=$HOME/.0gchaind/galileo
+ExecStart=/usr/local/bin/0gchaind start \
+  --chain-spec devnet \
+  --home $HOME/.0gchaind/galileo/0g-home/0gchaind-home \
+  --kzg.trusted-setup-path=$HOME/.0gchaind/galileo/kzg-trusted-setup.json \
+  --engine.jwt-secret-path=$HOME/.0gchaind/galileo/jwt-secret.hex \
+  --kzg.implementation=crate-crypto/go-kzg-4844 \
+  --home=$HOME/.0gchaind/galileo/0g-home/0gchaind-home \
+  --p2p.seeds=85a9b9a1b7fa0969704db2bc37f7c100855a75d9@8.218.88.60:26656 \
+  --p2p.external_address=$(curl -4 -s ifconfig.me):${OG_PORT}656
+Restart=always
+RestartSec=3
 LimitNOFILE=65535
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
-### Start service and run node
-echo ""
-printColor blue "[6/6] Start service and run node"
+  sudo tee /etc/systemd/system/geth.service > /dev/null <<EOF
+[Unit]
+Description=0g Geth Node Service
+After=network-online.target
 
-sudo systemctl daemon-reload
-sudo systemctl enable 0gchaind
-sudo systemctl restart 0gchaind && sudo journalctl -u 0gchaind -fo cat
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.0gchaind/galileo
+ExecStart=/usr/local/bin/geth \
+  --config $HOME/.0gchaind/galileo/geth-config.toml \
+  --datadir $HOME/.0gchaind/galileo/0g-home/geth-home \
+  --networkid 16601 \
+  --http.port ${OG_PORT}545 \
+  --ws.port ${OG_PORT}546 \
+  --authrpc.port ${OG_PORT}551 \
+  --bootnodes enode://de7b86d8ac452b1413983049c20eafa2ea0851a3219c2cc12649b971c1677bd83fe24c5331e078471e52a94d95e8cde84cb9d866574fec957124e57ac6056699@8.218.88.60:30303 \
+  --port ${OG_PORT}303 \
+  --networkid 16601
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
 
-### Useful commands
-printDelimiter
-printGreen "Переглянути журнал логів:         journalctl -u 0gchaind -fo cat "
-printGreen "Переглянути статус синхронізації: 0gchaind status | jq | grep \"catching_up\""
-source $HOME/.bash_profile
-printDelimiter
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  printGreen "Starting node..."
+  sudo systemctl daemon-reload
+  sudo systemctl enable 0gchaind
+  sudo systemctl enable geth
+  sudo systemctl start 0gchaind
+  sudo systemctl start geth
+
+  printGreen "✅ Installation complete."
 }
 
 install
