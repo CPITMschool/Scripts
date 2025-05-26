31f3ed7fc7de140e7c70f11c527f8d4e7fae63ec
Fearless Oleksii <143862878+0xFearless-1@users.noreply.github.com>
2025-05-26 10:36:14 +0300

    Update Delete-0G.sh

--- 0G/Delete-0G.sh
+++ 0G/Delete-0G.sh
@@ -9,30 +9,30 @@ function logo() {
 }
 
 function delete() {
-  printGreen "Stopping services..."
+  printGreen "Зупинка ноди..."
   sudo systemctl stop 0gchaind 0ggeth || true
 
-  printGreen "Disabling services..."
+  printGreen "Выдключення сервісного файлу..."
   sudo systemctl disable 0gchaind 0ggeth || true
 
-  printGreen "Removing systemd service files..."
+  printGreen "Видалення systemd сервісних файлів..."
   sudo rm -f /etc/systemd/system/0gchaind.service
   sudo rm -f /etc/systemd/system/0ggeth.service
 
-  printGreen "Reloading systemd daemon..."
+  printGreen "Перезавантаження systemd daemon..."
   sudo systemctl daemon-reload
 
-  printGreen "Removing binaries..."
+  printGreen "Видалення бінарних файлів"
   rm -f $HOME/go/bin/0gchaind
   rm -f $HOME/go/bin/geth
 
-  printGreen "Removing node data and configs..."
+  printGreen "Видалення всіх данних..."
   rm -rf $HOME/.0gchaind
   rm -rf $HOME/galileo-used
   rm -rf $HOME/galileo
   rm -rf $HOME/galileo.tar.gz
 
-  printGreen "Removing environment variables from .bash_profile..."
+  printGreen "Видалення значень .bash_profile..."
   sed -i '/MONIKER/d' $HOME/.bash_profile
   sed -i '/OG_PORT/d' $HOME/.bash_profile
   sed -i '/galileo-used\/bin/d' $HOME/.bash_profile
