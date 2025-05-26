50a8035e0f6d9e4c212d1e975fa431b1bbf8d396
Fearless Oleksii <andrienkoj7@gmail.com>
2025-05-26 06:10:48 +0300

    update install 0g

--- 0G/Install-0G.sh
+++ 0G/Install-0G.sh
@@ -8,89 +8,87 @@ function printGreen {
   echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
-printGreen "Installing dependencies..."
-source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/dependencies.sh)
-
-function install() {
   clear
   source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+  
+function install() {
+  # Введення змінних
+  read -p "Enter your MONIKER (default: test): " MONIKER
+  MONIKER=${MONIKER:-test}
 
-  printGreen "Updating packages and installing dependencies..."
-  sudo apt update && sudo apt upgrade -y
-  sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+  read -p "Enter your OG_PORT (default: 26): " OG_PORT
+  OG_PORT=${OG_PORT:-26}
 
-printGreen "Installing Go..."
-cd $HOME
-VER="1.21.3"
-wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
-rm "go$VER.linux-amd64.tar.gz"
-[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
-echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
-source $HOME/.bash_profile
-[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+  printGreen "Installing Go..."
+  cd $HOME
+  VER="1.21.3"
+  wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+  rm "go$VER.linux-amd64.tar.gz"
+  [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+  echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+  source $HOME/.bash_profile
+  [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
-printGreen "Setting environment variables..."
-read -p "Enter your moniker name: " MONIKER
-read -p "Enter custom port prefix (e.g. 26): " OG_PORT
-echo "export MONIKER=\"$MONIKER\"" >> $HOME/.bash_profile
-echo "export OG_PORT=\"$OG_PORT\"" >> $HOME/.bash_profile
-echo 'export PATH=$PATH:$HOME/galileo-used/bin' >> $HOME/.bash_profile
-source $HOME/.bash_profile
+  printGreen "Setting environment variables..."
+  echo "export MONIKER=\"$MONIKER\"" >> $HOME/.bash_profile
+  echo "export OG_PORT=\"$OG_PORT\"" >> $HOME/.bash_profile
+  echo 'export PATH=$PATH:$HOME/galileo-used/bin' >> $HOME/.bash_profile
+  source $HOME/.bash_profile
 
-printGreen "Downloading binaries..."
-cd $HOME
-rm -rf galileo
-wget -O galileo.tar.gz https://github.com/0glabs/0gchain-NG/releases/download/v1.1.1/galileo-v1.1.1.tar.gz
-tar -xzvf galileo.tar.gz -C $HOME
-rm -rf $HOME/galileo.tar.gz
-chmod +x $HOME/galileo/bin/geth
-chmod +x $HOME/galileo/bin/0gchaind
-cp $HOME/galileo/bin/geth $HOME/go/bin/geth
-cp $HOME/galileo/bin/0gchaind $HOME/go/bin/0gchaind
-mv $HOME/galileo $HOME/galileo-used
+  printGreen "Downloading binaries..."
+  cd $HOME
+  rm -rf galileo
+  wget -O galileo.tar.gz https://github.com/0glabs/0gchain-NG/releases/download/v1.1.1/galileo-v1.1.1.tar.gz
+  tar -xzvf galileo.tar.gz -C $HOME
+  rm -rf $HOME/galileo.tar.gz
+  chmod +x $HOME/galileo/bin/geth
+  chmod +x $HOME/galileo/bin/0gchaind
+  cp $HOME/galileo/bin/geth $HOME/go/bin/geth
+  cp $HOME/galileo/bin/0gchaind $HOME/go/bin/0gchaind
+  mv $HOME/galileo $HOME/galileo-used
 
-printGreen "Initializing node..."
-mkdir -p $HOME/.0gchaind
-cp -r $HOME/galileo-used/0g-home $HOME/.0gchaind
+  printGreen "Initializing node..."
+  mkdir -p $HOME/.0gchaind
+  cp -r $HOME/galileo-used/0g-home $HOME/.0gchaind
 
-geth init --datadir $HOME/.0gchaind/0g-home/geth-home $HOME/galileo-used/genesis.json
-0gchaind init $MONIKER --home $HOME/.0gchaind/tmp
-mv $HOME/.0gchaind/tmp/data/priv_validator_state.json $HOME/.0gchaind/0g-home/0gchaind-home/data/
-mv $HOME/.0gchaind/tmp/config/node_key.json $HOME/.0gchaind/0g-home/0gchaind-home/config/
-mv $HOME/.0gchaind/tmp/config/priv_validator_key.json $HOME/.0gchaind/0g-home/0gchaind-home/config/
-rm -rf $HOME/.0gchaind/tmp
+  geth init --datadir $HOME/.0gchaind/0g-home/geth-home $HOME/galileo-used/genesis.json
+  0gchaind init $MONIKER --home $HOME/.0gchaind/tmp
+  mv $HOME/.0gchaind/tmp/data/priv_validator_state.json $HOME/.0gchaind/0g-home/0gchaind-home/data/
+  mv $HOME/.0gchaind/tmp/config/node_key.json $HOME/.0gchaind/0g-home/0gchaind-home/config/
+  mv $HOME/.0gchaind/tmp/config/priv_validator_key.json $HOME/.0gchaind/0g-home/0gchaind-home/config/
+  rm -rf $HOME/.0gchaind/tmp
 
-sed -i -e "s/^moniker *=.*/moniker = \"$MONIKER\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
+  sed -i -e "s/^moniker *=.*/moniker = \"$MONIKER\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
 
-printGreen "Configuring ports..."
-sed -i "s/HTTPPort = .*/HTTPPort = ${OG_PORT}545/" $HOME/galileo-used/geth-config.toml
-sed -i "s/WSPort = .*/WSPort = ${OG_PORT}546/" $HOME/galileo-used/geth-config.toml
-sed -i "s/AuthPort = .*/AuthPort = ${OG_PORT}551/" $HOME/galileo-used/geth-config.toml
-sed -i "s/ListenAddr = .*/ListenAddr = \":${OG_PORT}303\"/" $HOME/galileo-used/geth-config.toml
-sed -i "s/^# *Port = .*/# Port = ${OG_PORT}901/" $HOME/galileo-used/geth-config.toml
-sed -i "s/^# *InfluxDBEndpoint = .*/# InfluxDBEndpoint = \"http:\/\/localhost:${OG_PORT}086\"/" $HOME/galileo-used/geth-config.toml
+  printGreen "Configuring ports..."
+  sed -i "s/HTTPPort = .*/HTTPPort = ${OG_PORT}545/" $HOME/galileo-used/geth-config.toml
+  sed -i "s/WSPort = .*/WSPort = ${OG_PORT}546/" $HOME/galileo-used/geth-config.toml
+  sed -i "s/AuthPort = .*/AuthPort = ${OG_PORT}551/" $HOME/galileo-used/geth-config.toml
+  sed -i "s/ListenAddr = .*/ListenAddr = \":${OG_PORT}303\"/" $HOME/galileo-used/geth-config.toml
+  sed -i "s/^# *Port = .*/# Port = ${OG_PORT}901/" $HOME/galileo-used/geth-config.toml
+  sed -i "s/^# *InfluxDBEndpoint = .*/# InfluxDBEndpoint = \"http:\/\/localhost:${OG_PORT}086\"/" $HOME/galileo-used/geth-config.toml
 
-sed -i.bak -e "s%:26658%:${OG_PORT}658%g;
-s%:26657%:${OG_PORT}657%g;
-s%:6060%:${OG_PORT}060%g;
-s%:26656%:${OG_PORT}656%g;
-s%:26660%:${OG_PORT}660%g" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
+  sed -i.bak -e "s%:26658%:${OG_PORT}658%g;
+  s%:26657%:${OG_PORT}657%g;
+  s%:6060%:${OG_PORT}060%g;
+  s%:26656%:${OG_PORT}656%g;
+  s%:26660%:${OG_PORT}660%g" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
 
-sed -i "s/address = \".*:3500\"/address = \"127.0.0.1:${OG_PORT}500\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
-sed -i "s/^rpc-dial-url *=.*/rpc-dial-url = \"http:\/\/localhost:${OG_PORT}551\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
+  sed -i "s/address = \".*:3500\"/address = \"127.0.0.1:${OG_PORT}500\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
+  sed -i "s/^rpc-dial-url *=.*/rpc-dial-url = \"http:\/\/localhost:${OG_PORT}551\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
 
-sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
-sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
+  sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
+  sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
+  sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
+  sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.0gchaind/0g-home/0gchaind-home/config/app.toml
 
-ln -sf $HOME/.0gchaind/0g-home/0gchaind-home/config/client.toml $HOME/.0gchaind/config/client.toml
+  ln -sf $HOME/.0gchaind/0g-home/0gchaind-home/config/client.toml $HOME/.0gchaind/config/client.toml
 
-printGreen "Creating systemd services..."
+  printGreen "Creating systemd services..."
 
-sudo tee /etc/systemd/system/0ggeth.service > /dev/null <<EOF
+  sudo tee /etc/systemd/system/0ggeth.service > /dev/null <<EOF
 [Unit]
 Description=0g Geth Node Service
 After=network-online.target
@@ -115,7 +113,7 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-sudo tee /etc/systemd/system/0gchaind.service > /dev/null <<EOF
+  sudo tee /etc/systemd/system/0gchaind.service > /dev/null <<EOF
 [Unit]
 Description=0gchaind Node Service
 After=network-online.target
@@ -146,33 +144,38 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
+  printGreen "Enabling and starting services..."
+  sudo systemctl daemon-reload
+  sudo systemctl enable 0gchaind 0ggeth
+  sudo systemctl restart 0gchaind 0ggeth
 
-# install dependencies, and disable statesync to avoid sync issues
-sudo apt install curl tmux jq lz4 unzip -y
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1false|" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
+  printGreen "Waiting for logs..."
+  sudo journalctl -u 0gchaind -u 0ggeth -f --no-hostname -o cat
 
-# stop node and backup priv_validator_state.json
-sudo systemctl stop 0gchaind 0ggeth
-cp $HOME/.0gchaind/0g-home/0gchaind-home/data/priv_validator_state.json $HOME/priv_validator_state.json.backup
+  printGreen "Downloading and applying snapshots..."
 
-# remove old data and unpack 0G snapshot
-rm -rf $HOME/.0gchaind/0g-home/0gchaind-home/data
-curl https://server-3.itrocket.net/testnet/og/og_2025-05-25_858889_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/0gchaind-home
+  # install dependencies, and disable statesync to avoid sync issues
+  sudo apt install curl tmux jq lz4 unzip -y
+  sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1false|" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
 
-# restore priv_validator_state.json
-mv $HOME/priv_validator_state.json.backup $HOME/.0gchaind/0g-home/0gchaind-home/data/priv_validator_state.json
+  # stop node and backup priv_validator_state.json
+  sudo systemctl stop 0gchaind 0ggeth
+  cp $HOME/.0gchaind/0g-home/0gchaind-home/data/priv_validator_state.json $HOME/priv_validator_state.json.backup
 
-# delete geth data and unpack Geth snapshot
-rm -rf $HOME/.0gchaind/0g-home/geth-home/geth/chaindata
-curl https://server-3.itrocket.net/testnet/og/geth_og_2025-05-25_858889_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/geth-home/geth
+  # remove old data and unpack 0G snapshot
+  rm -rf $HOME/.0gchaind/0g-home/0gchaind-home/data
+  curl https://server-3.itrocket.net/testnet/og/og_2025-05-26_901847_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/0gchaind-home
 
-printGreen "Enabling services..."
-sudo systemctl daemon-reload
-sudo systemctl enable 0gchaind 0ggeth
-sudo systemctl restart 0gchaind 0ggeth
-sudo journalctl -u 0gchaind -u 0ggeth -f --no-hostname -o cat
+  # restore priv_validator_state.json
+  mv $HOME/priv_validator_state.json.backup $HOME/.0gchaind/0g-home/0gchaind-home/data/priv_validator_state.json
 
-  printGreen "✅ Installation complete."
+  # delete geth data and unpack Geth snapshot
+  rm -rf $HOME/.0gchaind/0g-home/geth-home/geth/chaindata
+  curl https://server-3.itrocket.net/testnet/og/geth_og_2025-05-26_901847_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/geth-home/geth
+
+  # restart node and check logs
+  sudo systemctl restart 0gchaind 0ggeth
+  sudo journalctl -u 0gchaind -u 0ggeth -f
 }
 
 install
