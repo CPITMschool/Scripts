ac2304d234b20dd116d6cad512d2bbcdb0982aec
Fearless Oleksii <andrienkoj7@gmail.com>
2025-06-05 09:47:28 +0300

    update install 0g

--- 0G/Install-0G.sh
+++ 0G/Install-0G.sh
@@ -166,7 +166,7 @@ EOF
 
   printGreen "Downloading and applying snapshots..."
 
- # install dependencies, and disable statesync to avoid sync issues
+# install dependencies, and disable statesync to avoid sync issues
 sudo apt install curl tmux jq lz4 unzip -y
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1false|" $HOME/.0gchaind/0g-home/0gchaind-home/config/config.toml
 
@@ -176,14 +176,15 @@ cp $HOME/.0gchaind/0g-home/0gchaind-home/data/priv_validator_state.json $HOME/pr
 
 # remove old data and unpack 0G snapshot
 rm -rf $HOME/.0gchaind/0g-home/0gchaind-home/data
-curl https://server-3.itrocket.net/testnet/og/og_2025-05-29_1104753_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/0gchaind-home
+curl https://server-3.itrocket.net/testnet/og/og_2025-06-05_1519703_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/0gchaind-home
 
 # restore priv_validator_state.json
 mv $HOME/priv_validator_state.json.backup $HOME/.0gchaind/0g-home/0gchaind-home/data/priv_validator_state.json
 
 # delete geth data and unpack Geth snapshot
 rm -rf $HOME/.0gchaind/0g-home/geth-home/geth/chaindata
-curl https://server-3.itrocket.net/testnet/og/geth_og_2025-05-29_1104753_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/geth-home/geth
+curl https://server-3.itrocket.net/testnet/og/geth_og_2025-06-05_1519703_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchaind/0g-home/geth-home/geth
+
 
 # restart node and check logs
 sudo systemctl restart 0gchaind 0ggeth
