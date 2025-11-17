b8716f235b486254b077ab0c0a2f08838c8e663a
Fearless Oleksii <andrienkoj7@gmail.com>
2025-11-17 17:15:56 +0200

    add Stable

---
+++ Stable/Install-Stable.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+function printDelimiter {
+  echo "==========================================="
+}
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/dependencies.sh)
+
+function install() {
+clear
+source <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+
+echo -e "\e[30;47m Введіть ім'я moniker(Наприклад: Oliver):\e[0m"
+echo -en ">>> "
+read -r NODE_MONIKER
+
+### Install Dependencies
+
+### Building binaries
+echo ""
+printColor blue "[4/6] Building binaries"
+
+cd $HOME
+rm -rf bin
+mkdir bin && cd bin
+wget https://stable-testnet-data.s3.us-east-1.amazonaws.com/stabled-latest-linux-amd64-testnet.tar.gz
+tar -xvzf stabled-0.8.1-testnet-linux-amd64.tar.gz
+chmod +x stabled
+mv $HOME/bin/stabled $HOME/go/bin
+stabled version
+
+
+wardend init "$NODE_MONIKER" --chain-id stabletestnet_2201-1
+
+### Download genesis and addrbook
+# Create backup of default genesis
+mv ~/.stabled/config/genesis.json ~/.stabled/config/genesis.json.backup
+
+# Download testnet genesis
+wget https://stable-testnet-data.s3.us-east-1.amazonaws.com/stable_testnet_genesis.zip
+unzip stable_testnet_genesis.zip
+
+# Move genesis to config directory
+cp genesis.json ~/.stabled/config/genesis.json
+
+# Verify genesis checksum
+sha256sum ~/.stabled/config/genesis.json
+# Expected: 66afbb6e57e6faf019b3021de299125cddab61d433f28894db751252f5b8eaf2
+
+# Download optimized configuration
+wget https://stable-testnet-data.s3.us-east-1.amazonaws.com/rpc_node_config.zip
+unzip rpcnode_config.zip
+
+# Backup original config
+cp ~/.stabled/config/config.toml ~/.stabled/config/config.toml.backup
+
+# Apply new configuration
+cp config.toml ~/.stabled/config/config.toml
+
+# Update moniker in config
+sed -i "s/^moniker = \".*\"/moniker = \"$MONIKER\"/" ~/.stabled/config/config.toml
+
+# Set seeds and Peers
+SEEDS=""
+PEERS="5ed0f977a26ccf290e184e364fb04e268ef16430@37.187.147.27:26656,128accd3e8ee379bfdf54560c21345451c7048c7@37.187.147.22:26656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.stabled/config/config.toml
+
+FILE_APP=~/.stabled/config/app.toml
+FILE_CONFIG=~/.stabled/config/config.toml
+
+# --- Редагування app.toml ---
+echo "Редагування $FILE_APP..."
+# Повне додавання/заміна блоку [json-rpc]
+sed -i '/^# Enable JSON-RPC for EVM compatibility/,/^allow-unprotected-txs =/d; $a\# Enable JSON-RPC for EVM compatibility\n[json-rpc]\nenable = true\naddress = "0.0.0.0:8545"\nws-address = "0.0.0.0:8546"\nallow-unprotected-txs = true' $FILE_APP
+
+# --- Редагування config.toml ---
+echo "Редагування $FILE_CONFIG..."
+# P2P
+sed -i 's/^max_num_inbound_peers = .*/max_num_inbound_peers = 50/' $FILE_CONFIG
+sed -i 's/^max_num_outbound_peers = .*/max_num_outbound_peers = 30/' $FILE_CONFIG
+sed -i 's/persistent_peers = ""/persistent_peers = "5ed0f977a26ccf290e184e364fb04e268ef16430@37.187.147.27:26656,128accd3e8ee379bfdf54560c21345451c7048c7@37.187.147.22:26656"/' $FILE_CONFIG
+sed -i 's/^pex = .*/pex = true/' $FILE_CONFIG
+
+# RPC
+sed -i 's|^laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://0.0.0.0:26657"|' $FILE_CONFIG
+sed -i 's/^max_open_connections = .*/max_open_connections = 900/' $FILE_CONFIG
+sed -i 's/^cors_allowed_origins = .*/cors_allowed_origins = ["*"]/' $FILE_CONFIG
+
+echo "Редагування завершено."
+
+
+### Create service
+sudo tee /etc/systemd/system/stabled.service > /dev/null <<EOF
+[Unit]
+Description=Stable Daemon Service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=/usr/bin/stabled start --chain-id stabletestnet_2201-1
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+StandardOutput=journal
+StandardError=journal
+SyslogIdentifier=stabled
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+### Start service and run node
+echo ""
+printColor blue "[6/6] Start service and run node"
+
+sudo systemctl daemon-reload
+sudo systemctl enable stabled.service
+sudo systemctl start stabled.service
+
+### Useful commands
+printDelimiter
+printGreen "Переглянути журнал логів:         sudo journalctl -u stabled -f -o cat"
+printGreen "Переглянути статус синхронізації: stabled status | jq | grep \"catching_up\""
+source $HOME/.bash_profile
+printDelimiter
+}
+
+install
